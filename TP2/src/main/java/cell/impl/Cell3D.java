68e34fd1c8e4de5916044b70b4941454eac69eeb
lgalende <lgalende@itba.edu.ar>
2022-04-01 17:32:19 -0300

    Cell3D equals and hashCode implemented

--- TP2/src/main/java/cell/impl/Cell3D.java
+++ TP2/src/main/java/cell/impl/Cell3D.java
@@ -2,6 +2,8 @@ package cell.impl;
 
 import cell.Cell;
 
+import java.util.Objects;
+
 public class Cell3D extends Cell {
     private int z;
     public Cell3D(int x, int y, int z, boolean alive) {
@@ -20,6 +22,17 @@ public class Cell3D extends Cell {
     @Override
     public String toString() {
         return String.format("%s %s %s", this.x, this.y, this.z);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        Cell3D cell = (Cell3D) o;
+        return super.equals(o) && z == cell.z;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y, z);
     }
 
 }
