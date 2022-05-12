43a26e99b756c0afc5831991f48864db62fe6393
lgalende <lgalende@itba.edu.ar>
2022-05-12 14:56:06 -0300

    plot ej2-3, read csv columns into lists modularized

--- plots/tp4/main.py
+++ plots/tp4/main.py
@@ -1,35 +1,76 @@
 import pandas
+import numpy as np
 
 import plots
 
 tp4_path = "../../TP4/outputs/"
+sys2_path = tp4_path + "system2/"
 
 
 def plots_system1():
     sys1_path = tp4_path + "system1/"
 
-    data = pandas.read_csv(sys1_path + "positions.csv")
+    col_names = ['t', 'beeman', 'verlet', 'gpc', 'analytical_solution']
+    lists = read_csv_columns_to_lists(sys1_path + "positions.csv", col_names)
+    plots.plot_solutions(lists[0], lists[1], lists[2], lists[3], lists[4])
+    # TODO: zoom in
+
+    col_names = ['dt', 'beeman', 'verlet', 'gpc']
+    lists = read_csv_columns_to_lists(sys1_path + "dt-mse.csv", col_names)
+    plots.plot_mse(lists[0], lists[1], lists[2], lists[3])
+
+
+def plots_system2():
+    # plots_2_1()
+    # plots_2_2()
+    plots_2_3()
+    # plots_2_4()
+
+
+def plots_2_1():
+    col_names = []
+    lists = read_csv_columns_to_lists(sys2_path + ".csv", col_names)
+
+
+def plots_2_2():
+    col_names = []
+    lists = read_csv_columns_to_lists(sys2_path + ".csv", col_names)
+
+
+def plots_2_3():
+    col_names = ['v0', 'left', 'right', 'up', 'down', 'absorb']
+    lists = read_csv_columns_to_lists(sys2_path + "percentages-v0.csv", col_names)
+    plots.percentages_per_v0(np.array(lists[0]), np.array(lists[1]), np.array(lists[2]),
+                             np.array(lists[3]), np.array(lists[4]), np.array(lists[5]))
+
+    # fake data
+    v0 = np.array([5e3, 1e4, 5e4])
+    left = np.array([0.4, 0.2, 0.1])
+    right = np.array([0.1, 0.2, 0.5])
+    up = np.array([0.05, 0, 0.05])
+    down = np.array([0, 0, 0.05])
+    absorb = np.array([0.45, 0.6, 0.3])
+
+    plots.percentages_per_v0(v0, left * 100, right * 100, up * 100, down * 100, absorb * 100)
+
+
+def plots_2_4():
+    col_names = []
+    lists = read_csv_columns_to_lists(sys2_path + ".csv", col_names)
+
+
+def read_csv_columns_to_lists(file_name, col_names):
+    data = pandas.read_csv(file_name)
+    lists = []
 
     # converting column data to list
-    time = data['t'].tolist()
-    beeman = data['beeman'].tolist()
-    verlet = data['verlet'].tolist()
-    gpc = data['gpc'].tolist()
-    analytical_solution = data['analytical_solution'].tolist()
+    for name in col_names:
+        lists.append(data[name].tolist())
 
-    plots.plot_solutions(time, beeman, verlet, gpc, analytical_solution)
-
-    data = pandas.read_csv(sys1_path + "dt-mse.csv")
-
-    # converting column data to list
-    dt = data['dt'].tolist()
-    beeman = data['beeman'].tolist()
-    verlet = data['verlet'].tolist()
-    gpc = data['gpc'].tolist()
-
-    plots.plot_mse(dt, beeman, verlet, gpc)
+    return lists
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     plots_system1()
+    # plots_system2()
--- plots/tp4/plots.py
+++ plots/tp4/plots.py
@@ -1,4 +1,5 @@
 from matplotlib import pyplot as plt
+import numpy as np
 
 
 def plot_solutions(time, beeman, verlet, gpc, analytical_solution):
@@ -10,6 +11,9 @@ def plot_solutions(time, beeman, verlet, gpc, analytical_solution):
     plt.xlabel('tiempo [s]')
     plt.ylabel('posición [m]')
     # plt.title(f'')
+
+    # plt.xlim(3.15059, 3.1506)
+    # plt.ylim(0.1046, 0.10469)
 
     plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
     plt.tight_layout()
@@ -27,6 +31,28 @@ def plot_mse(dt, beeman, verlet, gpc):
 
     plt.xscale("log")
     plt.yscale("log")
+
+    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    plt.tight_layout()
+    plt.show()
+
+
+def percentages_per_v0(v0, left, right, up, down, absorb):
+    width = 0.35
+
+    fig = plt.figure()
+    ind = np.arange(len(v0))
+
+    p1 = plt.bar(ind, left, width, label='Izquierda')
+    p2 = plt.bar(ind, right, width, bottom=left, label='Derecha')
+    p3 = plt.bar(ind, up, width, bottom=left+right, label='Arriba')
+    p4 = plt.bar(ind, down, width, bottom=left+right+up, label='Abajo')
+    p5 = plt.bar(ind, absorb, width, bottom=left+right+up+down, label='Absorbidas')
+
+    plt.xlabel('V0 [m/s]')
+    plt.ylabel('Porcentajes')
+    aux = [format(n, '.0e') for n in v0]
+    plt.xticks(ind, aux)
 
     plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
     plt.tight_layout()
